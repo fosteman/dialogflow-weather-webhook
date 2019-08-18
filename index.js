df53ef1cc08ebe6fc5747d0399ea3754f027c839
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-08-18 09:30:44 -0400

    Final Weather Report

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
 const processMessage = (request, respose) => {
     const agent = new WebhookClient({ request, respose });
     console.log('Dialogflow Request Received!');
@@ -21,21 +23,73 @@ const processMessage = (request, respose) => {
         agent.add(`Hey, welcome to Fosteman's Weather Teller !`);
         agent.add(new Suggestion(`Ask me what's the forecast for tomorrow !`));
     }
+    function requestWeatherForecast(agent) {
+        let city = request.body.queryResult.parameters['geo-city']; // a required parameter
+        let date = request.body.queryResult.parameters['date'];
+        const weatherRequestOptions = {
+            method: 'get',
+            params: {
+                format: 'json',
+                num_of_days: 1,
+                q: encodeURIComponent(city),
+                key: wwoApiKey,
+                date
+            },
+            timeout: 4500, //5s is Dialogflow's restriction
+            responseType: 'json'
+        };
+        const forecast = (location, time, cloud_cover, temp_C, wind, description) => {
+            return {
+                location,
+                time,
+                cloud_cover,
+                temp_C,
+                wind,
+                description
+            }
+        };
+        const weatherReport = ({
+                                   location,
+                                   time,
+                                   cloud_cover,
+                                   temp_C,
+                                   wind,
+                                   description
+                               }) =>
+            `Conditions in the ${location} at ${time}
+        are ${temp_C} degrees, wind speed is ${wind} kmph,
+        while cloud coverage is ${cloud_cover},
+        overall ${description}
+        `;
 
-    function weather(agent) {
+        let FinalWeatherReport = ''; //I wonder if I could return a string right from axios pipe..
+
         return new Promise((resolve, reject) => {
-            // Get target city and date
-            let city = request.body.queryResult.parameters['geo-city']; // a required parameter
-            let date = request.body.queryResult.parameters['date'];
-            console.log('Fetching weather forecast...');
-
-
+            axios.get(weatherAPIEndpoint, weatherRequestOptions)
+                .then(res => res.data.data)
+                .then(data => {
+                    // generate forecast
+                    let instance = forecast(
+                        data.request[0].query, //location
+                        data.current_condition[0].observation_time, //time
+                        data.current_condition[0].cloudcover, //cloud coverage
+                        data.current_condition[0].temp_C, // measured temperature
+                        data.current_condition[0].windspeedKmph, //wind
+                        data.current_condition[0].weatherDesc[0].value //description
+                    );
+                    FinalWeatherReport = weatherReport(instance);
+                    console.log('Final weather report: ', FinalWeatherReport);
+                    agent.add(FinalWeatherReport);
+                    return resolve();
+                })
+                .catch(rej => resolve(rej));
         });
     }
 
+
     let intentMap = new Map();
     intentMap.set('Default welcome intent', welcome); //fix the label
-    intentMap.set('weather', weather);
+    intentMap.set('weather', requestWeatherForecast);
     agent.handleRequest(intentMap);
 };
 
@@ -65,64 +119,3 @@ const server = app.listen(app.set('port'), () => {
     console.log(`Webhook is running → ${server.address()}:${server.address().port}`);
 });
 
-function requestWeatherForecast(city, date) {
-    const weatherRequestOptions = {
-        method: 'get',
-        params: {
-            format: 'json',
-            num_of_days: 1,
-            q: encodeURIComponent(city),
-            key: wwoApiKey,
-            date
-        },
-        timeout: 4500, //5s is Dialogflow's restriction
-        responseType: 'json'
-    };
-    const forecast = (location, time, cloud_cover, temp_C, wind, description) => {
-        return {
-            location,
-            time,
-            cloud_cover,
-            temp_C,
-            wind,
-            description
-        }
-    };
-    const weatherReport = ({
-                               location,
-                               time,
-                               cloud_cover,
-                               temp_C,
-                               wind,
-                               description
-                           }) =>
-        `Conditions in the ${location} at ${time}
-        are ${temp_C} degrees, wind speed is ${wind} kmph,
-        while cloud coverage is ${cloud_cover},
-        overall ${description}
-        `;
-
-    let FinalWeatherReport = ''; //I wonder if I could return a string right from axios pipe..
-
-    return new Promise((resolve, reject) => {
-        axios.get(weatherAPIEndpoint, weatherRequestOptions)
-            .then(res => res.data.data)
-            .then(data => {
-                // generate forecast
-                let instance = forecast(
-                    data.request[0].query, //location
-                    data.current_condition[0].observation_time, //time
-                    data.current_condition[0].cloudcover, //cloud coverage
-                    data.current_condition[0].temp_C, // measured temperature
-                    data.current_condition[0].windspeedKmph, //wind
-                    data.current_condition[0].weatherDesc[0].value //description
-                );
-                FinalWeatherReport = weatherReport(instance);
-                console.log('Final weather report: ', FinalWeatherReport);
-                agent.add(FinalWeatherReport);
-                return resolve();
-            })
-            .catch(rej => resolve(rej));
-    });
-
-}
